f4d42a0e2b700d75f79aef5f84ce59f7a28d9e33
Thaman-Taunk <65014366+Thaman-Taunk@users.noreply.github.com>
2020-05-10 03:40:24 +0530

    Create pos.py

---
+++ pos.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+i=0
+print('o/p of list1 is...')
+while i<len(list1):
+    if list1[i]>0:
+     print(list1[i])
+    i+=1
+i=0 
+print('o/p of list2 is...')
+while i<len(list2):
+    if list2[i]>0:
+     print(list2[i]) 
+    i+=1
